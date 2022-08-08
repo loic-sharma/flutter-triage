0bd85e760ef3fa43131b949e7981a4240830c611
Loic Sharma <sharma.loic@gmail.com>
2022-08-07 17:46:08 -0700

    Add GitHub token; reorder script

--- main.dart
+++ main.dart
@@ -5,6 +5,98 @@ import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart' show DateFormat;
 import 'package:uri/uri.dart' show UriBuilder;
 
+void main() async {
+  String? token = Platform.environment['GITHUB_TOKEN'];
+
+  var queries = const [
+    TriageQuery(
+      title: 'Engine pull requests',
+      repo: 'flutter/engine',
+      query: 'is:open is:pr label:"affects: desktop" sort:updated-asc',
+    ),
+    TriageQuery(
+      title: 'Framework pull requests',
+      repo: 'flutter/flutter',
+      query: 'is:open is:pr label:"a: desktop" sort:updated-asc',
+    ),
+    TriageQuery(
+      title: 'Bugs without priorities',
+      repo: 'flutter/flutter',
+      query: 'is:open is:issue label:"a: desktop" -label:P0 -label:P1 -label:P2 -label:P3 -label:P4 -label:p5 -label:p6',
+    ),
+    TriageQuery(
+      title: 'P0 bugs',
+      repo: 'flutter/flutter',
+      query: 'is:open is:issue label:"a: desktop" sort:updated-asc label:"P0"',
+    ),
+    TriageQuery(
+      title: 'P1 bugs',
+      repo: 'flutter/flutter',
+      query: 'is:open is:issue label:"a: desktop" sort:updated-asc label:"P1"',
+    ),
+    TriageQuery(
+      title: 'P2 bugs',
+      repo: 'flutter/flutter',
+      query: 'is:open is:issue label:"a: desktop" sort:updated-asc label:"P2"',
+    ),
+    TriageQuery(
+      title: 'Flakes',
+      repo: 'flutter/flutter',
+      query: 'is:open is:issue label:"a: desktop" sort:updated-asc label:"team: flakes"',
+    ),
+    TriageQuery(
+      title: 'Regressions',
+      repo: 'flutter/flutter',
+      query: 'is:open is:issue label:"a: desktop" sort:updated-asc label:"severe: regression"',
+    ),
+    TriageQuery(
+      title: 'Crashes',
+      repo: 'flutter/flutter',
+      query: 'is:open is:issue label:"a: desktop" sort:updated-asc label:"severe: crash"',
+    ),
+    TriageQuery(
+      title: 'Popular issues',
+      repo: 'flutter/flutter',
+      query: 'is:open is:issue label:"a: desktop" sort:reactions-+1-desc -label:"new feature" -label:"severe: new feature"',
+    ),
+    TriageQuery(
+      title: 'Popular features requests',
+      repo: 'flutter/flutter',
+      query: 'is:open is:issue label:"a: desktop" sort:reactions-+1-desc label:"new feature"',
+    ),
+  ];
+
+  var output = File('desktop.md').openWrite();
+
+  output.writeln('# Flutter desktop triage');
+  output.writeln(
+    'If you come across a bug that is unrelated to desktop app development, '
+    'remove the `a: desktop label` and leave a comment explaining why. '
+    'That will send it back to triage.'
+  );
+  output.writeln();
+  output.writeln('[Wiki instructions](https://github.com/flutter/flutter/wiki/triage#desktop).');
+
+  for (var triageQuery in queries) {
+    var issues = await searchIssues(
+      'repo:${triageQuery.repo} ${triageQuery.query}',
+      token: token
+    );
+
+    output.writeln();
+    writeIssues(
+      output,
+      triageQuery.title,
+      triageQuery.repo,
+      triageQuery.query,
+      issues,
+    );
+  }
+
+  await output.flush();
+  output.close();
+}
+
 class TriageQuery {
   const TriageQuery({
     required this.title,
@@ -15,6 +107,99 @@ class TriageQuery {
   final String title;
   final String repo;
   final String query;
+}
+
+Future<GitHubIssueSearchResponse> searchIssues(
+  String query, {
+    int perPage = 10,
+    String? token = null,
+  }) async {
+  // https://docs.github.com/en/rest/search#search-issues-and-pull-requests
+  var apiUri = Uri.parse('https://api.github.com/search/issues');
+  var builder = UriBuilder.fromUri(apiUri)
+    ..queryParameters['q'] = query
+    ..queryParameters['per_page'] = perPage.toString();
+
+  var uri = builder.build();
+  var headers = <String, String>{
+    'Accept': 'application/vnd.github.text-match+json',
+    if (token != null) 'Authorization': 'Bearer $token',
+  };
+
+  for (var attempt = 0; attempt < 3; attempt++) {
+    var response = await http.get(uri, headers: headers);
+
+    // Handle rate limit
+    if (response.statusCode == 403) {
+      // TODO: Verify this is actually rate limit exhaustion.
+      // TODO: Use response to minimize sleep.
+      // String? used = response.headers['x-ratelimit-used'];
+      // String? reset = response.headers['x-ratelimit-reset'];
+      // String? remaining = response.headers['x-ratelimit-remaining'];
+
+      print('Rate limit exceeded on attempt ${attempt + 1}, sleeping...');
+      await Future.delayed(Duration(seconds: 30));
+      continue;
+    }
+
+    if (response.statusCode != 200) {
+      throw 'GitHub search failed: ${response.statusCode} ${response.reasonPhrase}';
+    }
+
+    var json = jsonDecode(response.body);
+    return GitHubIssueSearchResponse.fromJson(json);
+  }
+
+  throw 'GitHub search failed';
+}
+
+void writeIssues(
+  IOSink output,
+  String title,
+  String repo,
+  String query,
+  GitHubIssueSearchResponse response,
+) {
+  var encodedQuery = Uri.encodeQueryComponent(query);
+  var queryUrl = 'https://github.com/$repo/issues?q=$encodedQuery';
+
+  output.writeln('## $title');
+  output.writeln();
+
+  output.writeln('[${response.totalCount} open]($queryUrl).');
+  output.writeln();
+
+  if (response.totalCount > 0) {
+    output.writeln('Name | Comments');
+    output.writeln('-- | --');
+
+    for (var item in response.items) {
+      var createdAt = DateFormat.yMMMMd().format(item.createdAt);
+      var labels = item.labels
+        .map((l) => '[`$l`](https://github.com/$repo/labels/${Uri.encodeComponent(l)})')
+        .join(', ');
+
+      output.writeln(
+        '[${item.title}](${item.url})'
+        '<br />'
+        '<sub>'
+          '$labels'
+          '<br />'
+          '[#${item.number}](${item.url}) opened on $createdAt '
+          'by [${item.user}](${item.userUrl})'
+        '</sub>'
+
+        ' | '
+
+        '💬 [${item.comments}](${item.url})'
+      );
+    }
+
+    if (response.totalCount > response.items.length || response.incompleteResults) {
+      output.writeln();
+      output.writeln('[See more...]($queryUrl)');
+    }
+  }
 }
 
 class GitHubIssueSearchResponse {
@@ -90,180 +275,4 @@ class GitHubIssueSearchItem {
   final DateTime updatedAt;
   final DateTime? closedAt;
   final String state;
-}
-
-void writeIssues(
-  IOSink output,
-  String title,
-  String repo,
-  String query,
-  GitHubIssueSearchResponse response,
-) {
-  var encodedQuery = Uri.encodeQueryComponent(query);
-  var queryUrl = 'https://github.com/$repo/issues?q=$encodedQuery';
-
-  output.writeln('## $title');
-  output.writeln();
-
-  output.writeln('[${response.totalCount} open]($queryUrl).');
-  output.writeln();
-
-  if (response.totalCount > 0) {
-    output.writeln('Name | Comments');
-    output.writeln('-- | --');
-
-    for (var item in response.items) {
-      var createdAt = DateFormat.yMMMMd().format(item.createdAt);
-      var labels = item.labels
-        .map((l) => '[`$l`](https://github.com/$repo/labels/${Uri.encodeComponent(l)})')
-        .join(', ');
-
-      output.writeln(
-        '[${item.title}](${item.url})'
-        '<br />'
-        '<sub>'
-          '$labels'
-          '<br />'
-          '[#${item.number}](${item.url}) opened on $createdAt '
-          'by [${item.user}](${item.userUrl})'
-        '</sub>'
-
-        ' | '
-
-        '💬 [${item.comments}](${item.url})'
-      );
-    }
-
-    if (response.totalCount > response.items.length || response.incompleteResults) {
-      output.writeln();
-      output.writeln('[See more...]($queryUrl)');
-    }
-  }
-}
-
-Future<GitHubIssueSearchResponse> searchIssues(
-  String query, {
-    int perPage = 10,
-  }) async {
-  // https://docs.github.com/en/rest/search#search-issues-and-pull-requests
-  var apiUri = Uri.parse('https://api.github.com/search/issues');
-  var builder = UriBuilder.fromUri(apiUri)
-    ..queryParameters['q'] = query
-    ..queryParameters['per_page'] = perPage.toString();
-  var uri = builder.build();
-
-  for (var attempt = 0; attempt < 3; attempt++) {
-    var response = await http.get(uri, headers: {
-      'Accept': 'application/vnd.github.text-match+json',
-    });
-
-    // Handle rate limit
-    if (response.statusCode == 403) {
-      // TODO: Verify this is actually rate limit exhaustion.
-      // TODO: Use response to minimize sleep.
-      // String? used = response.headers['x-ratelimit-used'];
-      // String? reset = response.headers['x-ratelimit-reset'];
-      // String? remaining = response.headers['x-ratelimit-remaining'];
-
-      print('Rate limit exceeded on attempt ${attempt + 1}, sleeping...');
-      await Future.delayed(Duration(seconds: 30));
-      continue;
-    }
-
-    if (response.statusCode != 200) {
-      throw 'GitHub search failed: ${response.statusCode} ${response.reasonPhrase}';
-    }
-
-    var json = jsonDecode(response.body);
-    return GitHubIssueSearchResponse.fromJson(json);
-  }
-
-  throw 'GitHub search failed';
-}
-
-void main() async {
-  var output = File('desktop.md').openWrite();
-
-  output.writeln('# Flutter desktop triage');
-  output.writeln(
-    'If you come across a bug that is unrelated to desktop app development, '
-    'remove the `a: desktop label` and leave a comment explaining why. '
-    'That will send it back to triage.'
-  );
-  output.writeln();
-  output.writeln('[Wiki instructions](https://github.com/flutter/flutter/wiki/triage#desktop).');
-
-  var queries = const [
-    TriageQuery(
-      title: 'Engine pull requests',
-      repo: 'flutter/engine',
-      query: 'is:open is:pr label:"affects: desktop" sort:updated-asc',
-    ),
-    TriageQuery(
-      title: 'Framework pull requests',
-      repo: 'flutter/flutter',
-      query: 'is:open is:pr label:"a: desktop" sort:updated-asc',
-    ),
-    TriageQuery(
-      title: 'Bugs without priorities',
-      repo: 'flutter/flutter',
-      query: 'is:open is:issue label:"a: desktop" -label:P0 -label:P1 -label:P2 -label:P3 -label:P4 -label:p5 -label:p6',
-    ),
-    TriageQuery(
-      title: 'P0 bugs',
-      repo: 'flutter/flutter',
-      query: 'is:open is:issue label:"a: desktop" sort:updated-asc label:"P0"',
-    ),
-    TriageQuery(
-      title: 'P1 bugs',
-      repo: 'flutter/flutter',
-      query: 'is:open is:issue label:"a: desktop" sort:updated-asc label:"P1"',
-    ),
-    TriageQuery(
-      title: 'P2 bugs',
-      repo: 'flutter/flutter',
-      query: 'is:open is:issue label:"a: desktop" sort:updated-asc label:"P2"',
-    ),
-    TriageQuery(
-      title: 'Flakes',
-      repo: 'flutter/flutter',
-      query: 'is:open is:issue label:"a: desktop" sort:updated-asc label:"team: flakes"',
-    ),
-    TriageQuery(
-      title: 'Regressions',
-      repo: 'flutter/flutter',
-      query: 'is:open is:issue label:"a: desktop" sort:updated-asc label:"severe: regression"',
-    ),
-    TriageQuery(
-      title: 'Crashes',
-      repo: 'flutter/flutter',
-      query: 'is:open is:issue label:"a: desktop" sort:updated-asc label:"severe: crash"',
-    ),
-    TriageQuery(
-      title: 'Popular issues',
-      repo: 'flutter/flutter',
-      query: 'is:open is:issue label:"a: desktop" sort:reactions-+1-desc -label:"new feature" -label:"severe: new feature"',
-    ),
-    TriageQuery(
-      title: 'Popular features requests',
-      repo: 'flutter/flutter',
-      query: 'is:open is:issue label:"a: desktop" sort:reactions-+1-desc label:"new feature"',
-    ),
-  ];
-
-  for (var triageQuery in queries) {
-    var issues = await searchIssues('repo:${triageQuery.repo} ${triageQuery.query}');
-
-    output.writeln();
-    writeIssues(
-      output,
-      triageQuery.title,
-      triageQuery.repo,
-      triageQuery.query,
-      issues,
-    );
-  }
-
-  await output.flush();
-  output.close();
 }
